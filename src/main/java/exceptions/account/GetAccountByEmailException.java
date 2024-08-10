f058d7c6ef7b23c5902388ff09b3035bf105cbe0
NikitaDmitruk <neomouz@icloud.com>
2024-08-10 15:48:40 +0300

    improved: GetAccountByUsernameException.java, GetAccountByEmailException.java

--- src/main/java/exceptions/account/GetAccountByEmailException.java
+++ src/main/java/exceptions/account/GetAccountByEmailException.java
@@ -1,7 +1,5 @@
 package exceptions.account;
 
-import java.sql.SQLException;
-
 public class GetAccountByEmailException extends RuntimeException {
     public GetAccountByEmailException(Throwable cause) {
         super("Ошибка при получении аккаунта по Email", cause);
--- src/main/java/exceptions/account/GetAccountByUsernameException.java
+++ src/main/java/exceptions/account/GetAccountByUsernameException.java
@@ -1,7 +1,5 @@
 package exceptions.account;
 
-import java.sql.SQLException;
-
 public class GetAccountByUsernameException extends RuntimeException {
     public GetAccountByUsernameException(Throwable cause) {
         super("Ошибка при получении аккаунта по Username", cause);
