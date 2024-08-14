0f1ab9914f2c94893e98743e73c7909ada5d3023
EvgenyYakimo8 <colos0117@gmail.com>
2024-08-14 14:10:18 +0300

    Создал слушатель для подключения драйвера при инициализации приложения. Добавил SneakyThrows в класс PostgresConnection

--- src/main/java/connection/PostgresConnection.java
+++ src/main/java/connection/PostgresConnection.java
@@ -1,29 +1,17 @@
 package connection;
 
-import exceptions.DatabaseConnectionException;
+import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class PostgresConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "bigbase";
 
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new DatabaseConnectionException(e);
-        }
-    }
-
+    @SneakyThrows
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            throw new DatabaseConnectionException(e);
-        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
---
+++ src/main/java/listener/ApplicationListener.java
@@ -0,0 +1,17 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ApplicationListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
