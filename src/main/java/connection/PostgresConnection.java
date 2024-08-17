0f2fff34f05361baf85ab1c2df6cd31fd5ee2549
EvgenyYakimo8 <colos0117@gmail.com>
2024-08-17 09:46:25 +0300

    Мелкие правки

--- src/main/java/connection/PostgresConnection.java
+++ src/main/java/connection/PostgresConnection.java
@@ -1,14 +1,18 @@
 package connection;
 
+import lombok.Getter;
 import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class PostgresConnection {
+    @Getter
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    @Getter
     private static final String USER = "postgres";
-    private static final String PASSWORD = "root";
+    @Getter
+    private static final String PASSWORD = "bigbase";
 
     @SneakyThrows
     public static Connection getConnection() {
